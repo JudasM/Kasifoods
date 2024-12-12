30a34bcc5940612bd01c4f49975cd94b4ebd7e4d
JudasM <judasmohlala2012@gmail.com>
2024-12-12 16:24:27 +0200

    changed the routes on the path

--- Client/src/app/app.routes.ts
+++ Client/src/app/app.routes.ts
@@ -3,14 +3,13 @@ import { LoginComponent } from './pages/login/login.component';
 import { HomeComponent } from './pages/home/home.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { HomeLayoutComponent } from './layouts/home-layout/home-layout.component';
 
 export const routes: Routes = [
-    {path: 'home',  component: HomeComponent, children: [
-        
-            {path: '', component: NavbarComponent},
-            {path: '', component: FooterComponent}
-        
-    ]},
+    {path: 'home',  component: HomeLayoutComponent},
     {path: 'login', component: LoginComponent},
+    {path: '404', component: NotFoundComponent},
+    {path: '**', redirectTo: '404', pathMatch:'full'}
 
 ];
