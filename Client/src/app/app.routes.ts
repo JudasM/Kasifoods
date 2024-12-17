75e2e39010a788e2f09370ec70978fc3464123c4
JudasM <judasmohlala2012@gmail.com>
2024-12-17 13:45:35 +0200

    added the register route to the page

--- Client/src/app/app.routes.ts
+++ Client/src/app/app.routes.ts
@@ -5,10 +5,12 @@ import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
 import { HomeLayoutComponent } from './layouts/home-layout/home-layout.component';
+import { RegisterComponent } from './pages/register/register.component';
 
 export const routes: Routes = [
     {path: 'home',  component: HomeLayoutComponent},
     {path: 'login', component: LoginComponent},
+    {path: 'register', component: RegisterComponent},
     {path: '404', component: NotFoundComponent},
     {path: '**', redirectTo: '404', pathMatch:'full'}
 
